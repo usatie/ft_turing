44166e2d30916da729970c8f7d9b16279b62edf0
Shun Usami <usatie@gmail.com>
2024-12-26 15:24:35 +0900

    Add a function to validate the description of the Turing machine

--- src/main.ml
+++ src/main.ml
@@ -1,21 +1,24 @@
+open Validater
+
 (* Main *)
 let main () =
-  let jsonfile, input = Argparse.parse_args Sys.argv in
-  let jsondata = Yojson.Basic.from_file jsonfile in
-  let tmd = Json_parser.description_of_json jsondata in
-  let result = Validater.validate_input tmd input in
-  match result with
-  | Ok input ->
-      Printer.print_description tmd;
-      let tm = Turing_machine.create_machine tmd input in
-      Turing_machine.run_machine tm
-  | Error err_msg -> Printf.printf "Validation failed: %s\n" err_msg
+  try
+    let jsonfile, input = Argparse.parse_args Sys.argv in
+    let jsondata = Yojson.Basic.from_file jsonfile in
+    let tmd = Json_parser.description_of_json jsondata in
+    Validater.validate_description tmd >>= fun valid_tmd ->
+    Validater.validate_input valid_tmd input >>= fun valid_input ->
+    Printer.print_description valid_tmd;
+    let tm = Turing_machine.create_machine tmd valid_input in
+    Turing_machine.run_machine tm;
+    Ok ()
+  with
+  | Yojson.Json_error msg -> Error ("Invalid json: " ^ msg)
+  | exn -> Error ("Unexpected error: " ^ Printexc.to_string exn)
 
 let () =
-  try main () with
-  | Yojson.Json_error msg ->
-      Printf.eprintf "Invalid json: %s\n" msg;
-      exit 1
-  | exn ->
-      Printf.eprintf "Error: %s\n" (Printexc.to_string exn);
+  match main () with
+  | Ok () -> ()
+  | Error msg ->
+      Printf.eprintf "Error: %s\n" msg;
       exit 1
--- src/validater.ml
+++ src/validater.ml
@@ -11,7 +11,12 @@ let does_not_contain_blank blank input =
   if String.contains input blank then Error "String contains blank character"
   else Ok input
 
+let blank_is_part_of_alphabet alphabet blank = List.mem blank alphabet
 let ( >>= ) = Result.bind
+
+let validate_description tmd =
+  if blank_is_part_of_alphabet tmd.alphabet tmd.blank then Ok tmd
+  else Error "Blank character must be part of the alphabet"
 
 let validate_input tmd input =
   input
