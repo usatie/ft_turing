f283cfebe5ed0ff33170bd5fdb40f338052199ad
Shun Usami <usatie@gmail.com>
2024-12-28 11:08:54 +0900

    Refactor is_palindrome.c

--- machines/2_is_palindrome/is_palindrome.c
+++ machines/2_is_palindrome/is_palindrome.c
@@ -1,27 +1,79 @@
 void is_palindrome(char *input) {
-	while (*input) {
-		let c = *input; // If this is expressed as state memory, it would be so lengthy...
-		*input = '\0';
-		input++;
-		// Move to the end of the string
-		while (*input) input++;
-		input--;
-		if (*input == '\0') {
-			break;
+	while (*input) {              // 1. main loop: read
+		let c = *input;           // (state memory)
+		*input = '\0';            // 1. main loop: write
+		input++;                  // 1. main loop: RIGHT
+		
+		while (*input) input++;   // 2. goto last character: read, RIGHT
+		input--;                  // 2. goto last character: LEFT
+
+		if (*input == '\0') {     // 3. check last character: read
+			break;                // 3. check last character: to_state(HALT), write
+		}                         // 3. check last character
+		if (c != *input) {        // 3. check last character
+			input++;              // 3. check last character
+			*input = 'n';         // 4. return false
+			return;               // 4. return false
 		}
-		if (c != *input) {
-			input++;
-			*input = 'n';
-			return;
-		}
-		*input = '\0';
-		// Go back to the beginning of the string
-		while (*input) input--;
-		input++;
+		*input = '\0';            // 3. check last character
+
+		while (*input) input--;   // 5. goto first character
+		input++;                  // 5. goto first character
 	}
 	*input = 'y';
 	return;
 }
+
+/*
+void is_palindrome(char *input) {
+1_main_loop:
+	if (*input == '\0') {		// read
+		*input = 'y';			// write
+	 	return;					// HALT
+	} else {					// read
+		let c = *input;			// (state memory)
+		*input = '\0';			// write
+		input++;				// RIGHT
+		goto 2_goto_last;		// to_state
+	}
+
+2_goto_last:
+	if (*input == '\0') {		// read
+		input--;				// LEFT
+		goto 3_check_last;		// to_state
+	} else {					// read
+		input++;				// RIGHT
+		goto 2_goto_last;		// to_state
+	}
+
+3_check_last:
+	if (*input == '\0') {		// read
+		*input = 'y';			// write
+	 	return;					// HALT
+	} else if (*input != c) {	// read
+		input++;				// RIGHT
+		goto exit.false			// to_state
+	} else {					// read
+		*input = '\0';			// write
+		input--;				// LEFT
+		goto 5_goto_first;		// to_state
+	}
+
+5_goto_first:
+	if (*input == '\0') {		// read
+		input++;				// RIGHT
+		goto 1_main_loop;		// to_state
+	} else {					// read
+		input++;				// RIGHT
+		goto 5_goto_first;		// to_state
+	}
+
+exit.false:
+	*input = 'n';				// write
+	return;						// HALT
+}
+*/
+
 /*
 {
 	"name"   : "is_palindrome",
