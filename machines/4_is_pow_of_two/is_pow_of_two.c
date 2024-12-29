f7013a27ecdb7109a7c21411b42ca456d5164e6a
Shun Usami <usatie@gmail.com>
2024-12-29 12:22:21 +0900

    Update the is_pow_of_two machine to restore the input before return

--- machines/4_is_pow_of_two/is_pow_of_two.c
+++ machines/4_is_pow_of_two/is_pow_of_two.c
@@ -7,35 +7,83 @@
 // ...     -> n
 // "00000000" -> y
 void is_pow_of_two(char *input) {
-	// Check if the input only contains '0's
+	// 1. Check empty input
+	if (*input == '\0') {
+		printf("n\n");
+		return;
+	}
+	// 2. Validate input (only contains '0's)
 	while (1) {
-		if (*input == '\0') {
+		if (*input == '0') {
+			input++;
+		} else if (*input == '\0') {
 			input--;
-		}
-		if (*input != '0') {
 			break;
 		}
 	}
-	// go to first character
+	// 3. Go back to the beginning of the input
 	while (1) {
-		if (*input == '\0') {
-			input--;
-		}
 		if (*input == '0') {
+			input--;
+		} else if (*input == '\0') {
 			input++;
 			break;
 		}
-		input--;
 	}
-	while (1) {
-		if (*input == '\0') {
-			printf("y\n");
-			return;
-		}
-		if (*input != '0') {
-			printf("n\n");
-			return;
-		}
-		input++;
-	}
+	// 4. Check if the input is a power of two
+	/*
+	 * if (streq(input, "0")) {
+	 *   goto exit.yes;
+	 * } else {
+	 *   consume_zero = true;
+	 *   while (1) {
+	 *     if (*input == '0') {
+	 *       if (consume_zero) *input = 'M';
+	 *       consume_zero = !consume_zero;
+	 *       input++;
+	 *     } else if (*input == 'M') {
+	 *       input++;
+	 *     } else if (*input == '\0') {
+	 *       input--;
+	 *       break;
+	 *     }
+	 *   }
+	 * }
+	*/
+
+restore.yes:
+  // Go back to the beginning of the input
+  while (*input) input--;
+  input++;
+  // Replace 'M' with '0'
+  while (*input) {
+	if (*input == 'M') *input = '0';
+	input++;
+  }
+  // Print 'y'
+  *input = 'y';
+  return;
+
+restore.no:
+  // Go back to the beginning of the input
+  while (*input) input--;
+  input++;
+  // Replace 'M' with '0'
+  while (*input) {
+	if (*input == 'M') *input = '0';
+	input++;
+  }
+  // Print 'n'
+  *input = 'n';
+  return;
+
+exit.yes:
+  while (*input) input++;
+  *input = 'y';
+  return;
+
+exit.no:
+  while (*input) input++;
+  *input = 'n';
+  return;
 }
